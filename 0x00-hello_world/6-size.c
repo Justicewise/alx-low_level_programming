e34989062cf4f2b0af1c33cfcb03139b86aac92f
Justicewise <justiceglobal1@gmail.com>
2022-10-14 02:23:16 -0700

    Sizing of my operating system

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,8 @@ int main(void)
 {
 printf("Size of char: %ldbyte(s)\n", sizeof(char));
 printf("Size of int: %ldbyte(s)\n", sizeof(int));
-printf("Size of long int: %ldbyte(s)", sizeof(long int));
-printf("Size of long long int: %ldbytpe(s)", sizeof(long long int));
-printf("Size of a float: %ld byte(s)", sizeof(float));
+printf("Size of long int: %ldbyte(s)\n", sizeof(long int));
+printf("Size of long long int: %ldbytpe(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
