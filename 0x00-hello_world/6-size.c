39dc64708b82904de53a2a5b4728c444bdad4740
Justicewise <justiceglobal1@gmail.com>
2022-10-14 03:25:45 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * DEscription: 'knowing the size of my system'
+ * DEscription:'knowing the size of my system'
  *
- * Return: always 0 (Sucess)
+ * Return:always 0 (Sucess)
  */
 
 int main(void)
@@ -14,6 +14,5 @@ printf("Size of int: %ld byte(s)\n", sizeof(int));
 printf("Size of long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
-
 return (0);
 }
