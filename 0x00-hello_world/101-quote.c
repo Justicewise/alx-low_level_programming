10bad5043e4625356c0df650c199460e7aefce90
Justicewise <justiceglobal1@gmail.com>
2022-10-14 04:26:07 -0700

    returning 1 and printing without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main-Entry point
  *
- * Description:printing to the staderr'
+ * Description:'printing to the staderr'
  *
  * Return: 1 if Sucess.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
