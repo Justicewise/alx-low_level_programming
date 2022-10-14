1fd43ef60ef6e1ade065ece3fda311a7d4a41130
Justicewise <justiceglobal1@gmail.com>
2022-10-14 04:20:08 -0700

    returning 1 and printing without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main-Entry point
  *
  * Description:printing to the staderr'
  *
- * Return:Alawys 0(Sucess)
+ * Return: 1 if Sucess.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
