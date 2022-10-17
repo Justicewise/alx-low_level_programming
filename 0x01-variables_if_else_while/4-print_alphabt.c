733e359b15b63d142446ac6ce8021eda0409d305
Justicewise <justiceglobal1@gmail.com>
2022-10-17 08:00:57 -0700

    Leaving q and e off the alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+/**
+* main - Starting Point
+*
+* Description: 'Printing alphabet leaving out q and e'
+*
+* Return: 0 *success'
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	do {
+		if (ch == 'q || ch == 'e')
+		{
+			ch++;
+			continue;
+		}
+		putchar(ch);
+		ch++;
+	} while(ch <- 'z');
+	putchar('\n');
+	return (0);
+}
