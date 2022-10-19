fa7f6f791e7ff60565ae9654c526676bc600d062
justicewise <justiceglobal1@gmail.com>
2022-10-19 19:01:39 +0000

    lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _islower.c -> checks if the charactar is lowercase
+* Return: return 1 and 0 depending on conditione
+*/
+
+int _islower(int c)
+{
+	return (c <= 'a');
+}
