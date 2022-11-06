b3827ed5ed17b62f51497358d300b6ffe7e744fa
Justicewise <justiceglobal1@gmail.com>
2022-11-05 23:46:07 -0700

    hot fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,33 @@
-#include "main.h"
+int squareroot(int n, int i);
 
 /**
-* _sqrt_recursion - Returns the natural square root of a number.
-* @n: The number to return the square root of
-* Return: If n has a natural square root - the natural square root of n.
-* If n does not have a natural square root - -1.
-**/
+ * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
+ *
+ * @n: input
+ * Return: Always 0 (Success)
+ */
 
 int _sqrt_recursion(int n)
 {
-	int root = 0;
-	
-	int final;
-
 	if (n < 0)
 		return (-1);
-	if (n == 1)
-		return (1);
-	final = _sqrt_recursion(n,root);
-	return (final);
+	else
+		return (squareroot(n, (n + 1) / 2));
+}
+
+/**
+ * squareroot - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
+
+int squareroot(int n, int i)
+{
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (squareroot(n, i - 1));
 }
