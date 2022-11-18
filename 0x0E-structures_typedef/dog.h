03d02f13afc4083f719192dec609641d729d519d
Justicewise <justiceglobal1@gmail.com>
2022-11-18 09:55:42 -0800

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
-* File: dog.h
-* Auth: Justice Godson
-* Desc: Header file that defines a new type struct dog.
-*/
-
 /**
-* struct dog - A new type describing a dog.
-* @name: The name of the dog.
-* @age: The age of the dog.
-* @owner: The owner of the dog.
+* struct dog - struct that stores some information of a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* Description: struct called "dog" that stores its name, its age
+* and the name of its owner.
 */
 
 struct dog
@@ -25,12 +21,6 @@ struct dog
 	char *owner;
 
 };
-
-/**
-*  * dog_t - Typedef for struct dog
-*/
-
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
