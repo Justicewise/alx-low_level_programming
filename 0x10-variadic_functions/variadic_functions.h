31dd244b1946ab99cc6378282ac1e26d41bf7df9
Justicewise <justiceglobal1@gmail.com>
2022-11-28 06:50:42 -0800

    hot fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,21 +1,23 @@
-#ifndef _FUNCTION_VARIADIC_H_
-#define _FUNCTION_VARIADIC_H_
+#ifndef _VARIADIC_FUNCTIONS_
+
+#define _VARIADIC_FUNCTIONS_
+
+
+
+#include <stdarg.h>
+
+#include <stdio.h>
+
+
 
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
-int _putchar(char c):
-#include<stdarg.h>
-/**
- * struct print - multiple choice print
- * @x: char Type of print
- * @T_func: funct
- */
-typedef struct print
-{
-	char *x;
-	void (*T_func)(va_list);
-} t_print;
+
+
 
 #endif
