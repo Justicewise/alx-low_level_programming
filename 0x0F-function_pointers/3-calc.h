5f2317bec48e346ab39196c8b923061232d972d6
Justicewise <justiceglobal1@gmail.com>
2022-11-28 05:18:58 -0800

    hot fixed

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,4 +1,3 @@
-
 #ifndef _CALC_H_
 #define _CALC_H_
 
@@ -12,8 +11,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,10 +1,10 @@
 #include "3-calc.h"
 
-/*
-* main - Run the program
+/**
+* main - check the code for Holberton School students.
 * @argc: argument count.
 * @argv: argument vector.
-* Return: Always 0.
+* Return: Always 0
 */
 
 int main(int argc, char *argv[])
@@ -23,16 +23,13 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 	operation = get_op_func(argv[2]);
-
 	if (operation == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-
 	printf("%d\n", operation(a, b));
 	return (0);
-}
+
